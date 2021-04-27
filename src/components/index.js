ec302b4b4b2609384f996c782baf130016f7b612
Tom Söderlund <tom@tomsoderlund.com>
2021-04-27 11:41:34 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as CardFormOneRow } from './CardFormOneRow'
-export { default as CardFormSplit } from './CardFormSplit'
-export { default as ContactInfoForm } from './ContactInfoForm'
-export { default as PaymentForm } from './PaymentForm'
+export { default as ContactInfoForm } from './common/ContactInfoForm'
+
+export { default as CardFormOneRow } from './stripe/CardFormOneRow'
+export { default as CardFormSplit } from './stripe/CardFormSplit'
+export { default as PaymentForm } from './stripe/PaymentForm'
