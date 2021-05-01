386c807f282641fae31e26d9bc24496aeced6583
Tom Söderlund <tom@tomsoderlund.com>
2021-05-01 09:33:04 +0200

    Move Stripe mockup backend

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -3,22 +3,4 @@
  * Based on https://dev.to/mattc/how-to-mock-api-calls-in-storybook-1762
  */
 
-require('dotenv').config() // for .env config
-
-const bodyParser = require('body-parser')
-
-const expressMiddleWare = (router) => {
-  router.use(bodyParser.urlencoded({ extended: false }))
-  router.use(bodyParser.json())
-
-  router.get('/api/payments/:paymentId', (req, res) => {
-    if (req.params.paymentId === 'error') {
-      res.status(500).send('Something broke!')
-    }
-    res.send({ data: { text: 'hello world' } })
-  })
-}
-
-module.exports = expressMiddleWare
-
-console.log(`\nSee mockup API on: http://localhost:6007/api/payments/123\nSTRIPE_APP_PUBLIC_KEY: ${process.env.STRIPE_APP_PUBLIC_KEY}\n`)
+module.exports = require('../stories/server/stripeServer')
