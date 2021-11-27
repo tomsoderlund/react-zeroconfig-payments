35f22bf2b4bc4bc291082361c1c45a89da726195
Tom Söderlund <tom@tomsoderlund.com>
2021-11-27 14:53:27 +0100

    Expiration Date

--- stories/_helpers/components/HelpInstructions.js
+++ stories/_helpers/components/HelpInstructions.js
@@ -4,7 +4,7 @@ const HelpInstructions = () => {
   return (
     <blockquote>
       Test card number: <strong>4242424242424242</strong>,
-      Date: <strong>01/{(new Date()).getYear() - 100 + 1}</strong>,
+      Expiration Date: <strong>01/{(new Date()).getYear() - 100 + 1}</strong>,
       CVC: <strong>100</strong>
     </blockquote>
   )
