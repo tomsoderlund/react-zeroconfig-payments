d58d0c13401ae848b0beef58eee2c38443d250b9
Tom Söderlund <tom@tomsoderlund.com>
2021-11-27 15:32:31 +0100

    ShowResponse ID

--- stories/_helpers/components/ShowResponse.js
+++ stories/_helpers/components/ShowResponse.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const ShowResponse = ({ response = {} }) => {
   if (!response || Object.keys(response).length === 0) return null
-  const { paymentMethod, paymentIntent, error } = response
+  const { paymentMethod, paymentIntent, error, id } = response
   return (
     <div
       className={'response' + (error ? ' error' : '')}
     >
       {error && error.message}
+      {id && `ID: ${id}`}
       {paymentMethod && 'Credit card accepted'}
       {paymentIntent && `paymentIntent ${paymentIntent.id} ${paymentIntent.status}`}
     </div>
